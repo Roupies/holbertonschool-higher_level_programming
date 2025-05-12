ab95f8a471ce20544e4647c2e860427b5c2d13bb
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-12 16:29:16 +0200

    fixed the first line of the fonction that could result in an IndexError

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-
-    value = my_list[idx]
-
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):  # Vérification de l'index avant tout accès
         return None
     else:
         return my_list[idx]
+
