34b5dc5700862c6f19fa89748912d0beb9b51ca9
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-13 14:29:20 +0200

    fix file task 6, the output was not accurate

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-
     for row in matrix:
-        print(" ".join(f"{x:3}" for x in row))
+        for i in range(len(row)):
+            if i < len(row) - 1:
+                print("{:d}".format(row[i]), end=" ")
+            else:
+                print("{:d}".format(row[i]), end="")
+        print()
