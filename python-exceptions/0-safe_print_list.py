18abd0f9b2e8a4dee731162500659932e6da8dc1
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-16 13:29:06 +0200

    fix the loops

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,17 +2,19 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    i = counter = 0
+    counter = 0
     try:
         if x <= 0:
             return 0
+        if isinstance(my_list, str):
+            my_list = list(my_list)
         for i in my_list:
             counter += 1
             print(i, end='')
-            if i == x:
-                print()
-                return counter
-        print()
-        return counter
-    except TypeError or ValueError:
+            if counter == x:
+                break
+    except (TypeError, ValueError):
         print("Please choose a positive integer for x")
+        return 0
+    print()
+    return counter
