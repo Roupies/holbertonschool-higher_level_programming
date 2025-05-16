9dc1ecc63ede05f2c3430c1d7a0222813149cc5e
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-16 13:44:54 +0200

    fix the index of the function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-
-    counter = 0
+    count = 0
     try:
-        if x <= 0:
-            return 0
-        if isinstance(my_list, str):
-            my_list = list(my_list)
-        for i in my_list:
-            counter += 1
-            print(i, end='')
-            if counter == x:
-                break
-    except (TypeError, ValueError):
-        print("Please choose a positive integer for x")
-        return 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
     print()
-    return counter
+    return count
