6e8406eee337184b8859e2a98c396d6f6b0294bd
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-16 16:02:42 +0200

    fix the indentation

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,17 +2,17 @@
 
 def safe_print_list_integers(my_list=[], x=0):
 
-    count = 0
-    try:
+        count = 0
         for i in my_list:
-            if isinstance(i, int):
-                print("{:d}".format(i), end='')
-                count += 1
-            else:
-                continue
-            if count == x:
-                break
-    except (TypeError, ValueError):
-        pass
-    print()
-    return count
+            try:
+                if isinstance(i, int):
+                    print("{:d}".format(i), end='')
+                    count += 1
+                else:
+                    continue
+                if count == x:
+                    break
+            except (TypeError, ValueError):
+                pass
+        print()
+        return count
