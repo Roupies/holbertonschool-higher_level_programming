ee425eea98c9db7cba084a0688664b54fe976ea5
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-16 10:43:41 +0200

    completion of task 0 + add every main.c and fonction files

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+    i = 0
+    try:
+        for i in my_list:
+            print(i, end='')
+            if i == x:
+                print()
+                return i
+        print()    
+        return i
+    except TypeError or ValueError:
+        print("Please choose a positive integer for x")
