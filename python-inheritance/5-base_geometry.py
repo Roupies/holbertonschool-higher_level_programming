99423f275ddec9b93624b03fcef17ac9e6788508
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-30 15:48:14 +0200

    fix pycodestyle for task 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 class BaseGeometry:
+    """
+    BaseGeometry is a base class representing
+    an abstract geometry.
+
+    It currently has no methods or attributes,
+    but it will serve as a parent class for
+    more specific geometry classes.
+    """
     pass
