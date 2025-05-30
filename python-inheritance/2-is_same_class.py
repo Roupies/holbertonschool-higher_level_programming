ae312cb285a3c6d3b92126e2204e9dc8d34869a5
‘Roupies’ <maxime.naguet@gmail.com>
2025-05-30 14:29:07 +0200

    change the length for task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python3
-
 def is_same_class(obj, a_class):
-    """Check if obj is exactly an instance of a_class (not a subclass)."""
+    """
+    Check if obj is exactly an instance of a_class (no subclasses).
+
+    Args:
+        obj: any object
+        a_class: class to check
+
+    Returns:
+        bool: True if obj is exactly instance of a_class, else False
+    """
     return type(obj) is a_class
