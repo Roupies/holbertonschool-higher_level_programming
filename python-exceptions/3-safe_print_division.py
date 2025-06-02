a20d55f8b2c65079f0af570b6d47b520e85d9aa9
‘Roupies’ <maxime.naguet@gmail.com>
2025-06-02 08:41:05 +0200

    completion of the rest of the task

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-
     try:
         result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally
+    except (ValueError, TypeError, ZeroDivisionError):
+        print("Inside result: None")
+        return None
+    else:
+        print("Inside result: {:0.1f}".format(result))
         return result
+    finally:
+        pass
