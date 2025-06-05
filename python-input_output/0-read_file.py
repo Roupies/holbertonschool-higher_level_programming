762fb7f5ee63e9aa741bf7e18a5b1f2f04719478
‘Roupies’ <maxime.naguet@gmail.com>
2025-06-05 10:08:43 +0200

    modified the docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """
-    Reads a text file (UTF8) and prints it to the POSIX standard output.
+    Reads a text file (UTF8) and prints it to stdout.
+    Uses the with statement for file handling.
+    Does not handle file permission or file doesn't exist exceptions.
 
-    Keyword arguments:
+    Args:
     filename -- the name of the file to read (default is an empty string)
     """
     with open(filename, 'r', encoding='utf-8') as file:
