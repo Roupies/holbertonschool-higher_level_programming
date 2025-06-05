20770427c0c0b906cf961eef0f3a2de0c1ea6b26
‘Roupies’ <maxime.naguet@gmail.com>
2025-06-05 10:03:56 +0200

    add utf8 for the read_file fonction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """
-    Reads a text file and prints it to the POSIX standard output.
+    Reads a text file (UTF8) and prints it to the POSIX standard output.
 
     Keyword arguments:
     filename -- the name of the file to read (default is an empty string)
     """
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
